1241548edeca1b67b4d8b072775e32fe98de7c07
Josh Thomas <joshthomas@google.com>
2016-02-25 02:40:40 -0700

    I would never show this to my coworkers...

--- app.js
+++ app.js
@@ -14,9 +14,64 @@ os.Process = function(index) {
 };
 
 
-os.Process.RUNNING = 'R';
+os.Process.prototype.reset_from_tick = function(tick) {
+  this.ticks.splice(tick);
+};
 
-os.Process.ARRIVED = 'A';
+
+os.Process.prototype.remaining_time = function() {
+  var executed_t = this.ticks.filter(function(v) {return v === 'R'}).length;
+
+  return this.service_time - executed_t;
+};
+
+
+os.Process.prototype.finished = function() {
+  return this.remaining_time() === 0;
+};
+
+
+os.Process.prototype.finish_time = function() {
+  if (!this.finished()) {
+    return '-';
+  }
+
+  for (var i = this.ticks.length; i >= 0; i--) {
+    if (this.ticks[i] === 'R') {
+      return i + 1;
+    }
+  }
+};
+
+
+os.Process.prototype.start_time = function() {
+  for (var i = 0; i < this.ticks.length; i++) {
+    if (this.ticks[i] === 'R') {
+      return i;
+    }
+  }
+
+  return null;
+};
+
+
+os.Process.prototype.turnaround_time = function() {
+  if (!this.finished()) {
+    return '-';
+  }
+
+  return this.finish_time() - this.arrival_time;
+};
+
+
+
+os.Process.prototype.response_time = function() {
+  if (this.start_time() === null) {
+    return '-';
+  }
+
+  return this.start_time() - this.arrival_time;
+};
 
 
 /**
@@ -41,7 +96,7 @@ os.ProcessListCtrl = function() {
   /**
    * @type {number}
    */
-  this.ticks = new Array(25);
+  this.ticks = new Array(50);
 
   // Create the initial array of processes.
   this.update_processes();
@@ -60,6 +115,37 @@ os.ProcessListCtrl.prototype.update_processes = function() {
   }
 
   this.processes = processes;
+};
+
+
+os.ProcessListCtrl.prototype.toggle_process = function(tick, pid) {
+  var p = this.processes[pid];
+
+  if (tick < p.arrival_time) {
+    return;
+  }
+
+  for (var i = 0; i < this.processes.length; i++) {
+    if (i === pid) {
+      continue;
+    }
+
+    this.processes[i].reset_from_tick(tick);
+  }
+
+  switch (p.ticks[tick]) {
+    case 'R':
+      p.ticks[tick] = 'D';
+      break;
+    case 'D':
+      p.ticks[tick] = null;
+      break;
+    default:
+      if (p.finished()) {
+        return;
+      }
+      p.ticks[tick] = 'R';
+  }
 };
 
 
